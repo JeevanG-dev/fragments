f185cc3d0b6466e9444ea7550ec7d76ed2c73058
Jeevan Gaudel <jatingaudel01@gmail.com>
2024-04-12 17:18:05 +1000

    modified some css module

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import FoodItems from "./component/FoodItems";
 import ErrorMessage from "./component/ErrorMessage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Container from "./component/Container";
 
 function App() {
    let vegItems = ["Tomato", "Cauli", "Potato", "lentils", " Onion"];
@@ -10,13 +11,16 @@ function App() {
   //passing data via props
 
   return (
-    <>
-      <div className="container">
+<Container>
+<div className="container">
         <h2 style={{ textAlign: "center" }}>Healthy Vegetables </h2>
         <ErrorMessage items = {vegItems}></ErrorMessage>
         <FoodItems items = {vegItems}></FoodItems>
       </div>
-    </>
+
+
+</Container>
+     
   );
 }
 
